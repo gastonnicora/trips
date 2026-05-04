7a50115d6b23c16fdb4318721a0a21b198897e66
gastonnicora <gastonmatias.21@gmail.com>
2026-05-04 16:21:17 -0300

    doc userput

--- src/main/java/com/gastonnicora/trips/dtos/request/User/UserPut.java
+++ src/main/java/com/gastonnicora/trips/dtos/request/User/UserPut.java
@@ -2,11 +2,35 @@ package com.gastonnicora.trips.dtos.request.User;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(description = "DTO de usuario para put")
+/**
+ * DTO para actualizar los datos básicos de un usuario.
+ * <p>
+ * Hereda de {@link UserBasic} e incluye los campos:
+ * <ul>
+ * <li>name: Nombre del usuario</li>
+ * <li>lastname: Apellido del usuario</li>
+ * <li>email: Correo electrónico del usuario</li>
+ * </ul>
+ * </p>
+ * <p>
+ * Se utiliza típicamente en los endpoints de actualización de usuarios (PUT).
+ * </p>
+ * 
+ * @author Gastón
+ * @version 1.0
+ * @since 2023-05-04
+ */
+@Schema(description = "DTO de usuario para actualización (PUT)")
 public class UserPut extends UserBasic {
 
+    /**
+     * Constructor completo.
+     * 
+     * @param name     Nombre del usuario
+     * @param lastname Apellido del usuario
+     * @param email    Email del usuario
+     */
     public UserPut(String name, String lastname, String email) {
         super(name, lastname, email);
     }
-
 }
