fda39bab4b101209cb9c21be8742d5e7e9d443fc
gastonnicora <gastonmatias.21@gmail.com>
2026-05-06 10:45:58 -0300

    test service set role

---
+++ src/test/java/com/gastonnicora/trips/unit/service/user/SetRoleTest.java
@@ -0,0 +1,103 @@
+package com.gastonnicora.trips.unit.service.user;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.gastonnicora.trips.dtos.entities.UserDTO;
+import com.gastonnicora.trips.dtos.request.User.UserChangeRole;
+import com.gastonnicora.trips.entities.User;
+import com.gastonnicora.trips.enums.Role;
+import com.gastonnicora.trips.exceptions.ErrorException;
+import com.gastonnicora.trips.mappers.UserMapper;
+import com.gastonnicora.trips.repositories.UserRepository;
+import com.gastonnicora.trips.services.UserService;
+
+import jakarta.transaction.Transactional;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@Transactional
+@ExtendWith(MockitoExtension.class)
+public class SetRoleTest {
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private UserMapper userMapper;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    void shouldAddUserRoleIfMissing() {
+        UUID uuid = UUID.randomUUID();
+
+        User user = new User("John", "Doe", "mail", "pass", new HashSet<>(Set.of(Role.ADMIN)));
+        user.setUuid(uuid);
+
+        UserChangeRole request = new UserChangeRole();
+        request.setRoles(new HashSet<>(Set.of(Role.ADMIN)));
+
+        when(userRepository.findByUuid(uuid)).thenReturn(Optional.of(user));
+        when(userMapper.toDTO(user)).thenReturn(new UserDTO());
+
+        userService.setRole(uuid, request);
+
+        assertTrue(user.getRole().contains(Role.USER)); 
+        assertEquals(Set.of(Role.ADMIN, Role.USER), user.getRole());
+    }
+
+    @Test
+    void shouldRemoveSuperAdminFromRequest() {
+        UUID uuid = UUID.randomUUID();
+
+        User user = new User("John", "Doe", "mail", "pass", Set.of(Role.USER));
+        user.setUuid(uuid);
+
+        UserChangeRole request = new UserChangeRole();
+        request.setRoles(new HashSet<>(Set.of(Role.ADMIN, Role.SUPER_ADMIN)));
+
+        when(userRepository.findByUuid(uuid)).thenReturn(Optional.of(user));
+        when(userMapper.toDTO(user)).thenReturn(new UserDTO());
+
+        userService.setRole(uuid, request);
+
+        assertFalse(user.getRole().contains(Role.SUPER_ADMIN));
+    }
+
+    @Test
+    void shouldThrowException_whenUserIsSuperAdmin() {
+        UUID uuid = UUID.randomUUID();
+
+        User user = new User("Super", "Admin", "mail", "pass",
+                Set.of(Role.SUPER_ADMIN));
+
+        when(userRepository.findByUuid(uuid)).thenReturn(Optional.of(user));
+
+        UserChangeRole request = new UserChangeRole(); // TODO 🚀:  agregar constructor vacio
+        request.setRoles(new HashSet<>(Set.of(Role.ADMIN)));
+
+        assertThrows(ErrorException.class, () -> {
+            userService.setRole(uuid, request);
+        });
+    }
+
+}
