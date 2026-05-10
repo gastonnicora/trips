7898554eb51898ccdf1789cacd4ab8a428ac3161
gastonnicora <gastonmatias.21@gmail.com>
2026-05-10 17:56:14 -0300

    test service deactivateAllByUserUuid

--- src/test/java/com/gastonnicora/trips/repository/UserRepositoryTest.java
+++ src/test/java/com/gastonnicora/trips/repository/UserRepositoryTest.java
@@ -7,30 +7,37 @@ import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.test.context.ActiveProfiles;
 
 import com.gastonnicora.trips.entities.User;
 import com.gastonnicora.trips.enums.Role;
 import com.gastonnicora.trips.repositories.UserRepository;
 
-import jakarta.transaction.Transactional;
-
 @ActiveProfiles("test")
-@SpringBootTest
-@Transactional
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
 
     @Test
-    void shouldFindByUsername() {
+    void shouldFindByEmail() {
 
-        User user = new User("test", "test", "test", "test", Set.of(Role.USER));
+        User user = new User(
+                "username",
+                "latName",
+                "test@test.com",
+                "password",
+                Set.of(Role.USER));
+
+        user.setEnabled(true);
+
         userRepository.save(user);
 
-        Optional<User> found = userRepository.findByEmailAndEnabledTrue("test");
+        Optional<User> found = userRepository.findByEmailAndEnabledTrue("test@test.com");
 
         assertTrue(found.isPresent());
     }
---
+++ src/test/java/com/gastonnicora/trips/unit/service/refreshToken/DeactivateAllByUserUuidTest.java
@@ -0,0 +1,67 @@
+package com.gastonnicora.trips.unit.service.refreshToken;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.gastonnicora.trips.entities.RefreshToken;
+import com.gastonnicora.trips.repositories.RefreshTokenRepository;
+import com.gastonnicora.trips.services.RefreshTokenService;
+
+@ExtendWith(MockitoExtension.class)
+public class DeactivateAllByUserUuidTest {
+    @Mock
+    private RefreshTokenRepository repo;
+
+    @InjectMocks
+    private RefreshTokenService service;
+
+    @Test
+    void shouldDeactivateAllTokens() {
+
+        UUID uuid = UUID.randomUUID();
+
+        RefreshToken token1 = new RefreshToken();
+        token1.setActive(true);
+
+        RefreshToken token2 = new RefreshToken();
+        token2.setActive(true);
+
+        when(repo.findAllByUserUuidAndActiveTrue(uuid))
+                .thenReturn(List.of(token1, token2));
+
+        service.deactivateAllByUserUuid(uuid);
+
+        assertFalse(token1.isActive());
+        assertFalse(token2.isActive());
+
+        verify(repo, times(2)).save(any(RefreshToken.class));
+    }
+    
+    @Test
+    void shouldDeactivateAllTokensIfEmptyList() {
+
+        UUID uuid = UUID.randomUUID();
+
+
+        when(repo.findAllByUserUuidAndActiveTrue(uuid))
+                .thenReturn(List.of());
+
+        service.deactivateAllByUserUuid(uuid);
+
+
+        verify(repo, never()).save(any(RefreshToken.class));
+    }
+}
