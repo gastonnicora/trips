2e0ad2d731e6d5a953f3bd7202e44a12d43805bf
gastonnicora <gastonmatias.21@gmail.com>
2026-05-20 11:55:22 -0300

    Test repo deleteByUserUuid

---
+++ src/test/java/com/gastonnicora/trips/repository/refreshToken/DeleteByUserUuidTest.java
@@ -0,0 +1,66 @@
+package com.gastonnicora.trips.repository.refreshToken;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.gastonnicora.trips.entities.RefreshToken;
+import com.gastonnicora.trips.entities.User;
+import com.gastonnicora.trips.enums.Role;
+import com.gastonnicora.trips.repositories.RefreshTokenRepository;
+import com.gastonnicora.trips.repositories.UserRepository;
+
+@ActiveProfiles("test")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class DeleteByUserUuidTest {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RefreshTokenRepository refreshTokenRepository;
+
+    @Test
+    void shouldDeleteByUserUuid() {
+        User user = new User(
+                "username",
+                "latName",
+                "test@test.com",
+                "password",
+                Set.of(Role.USER));
+
+        user.setEnabled(true);
+        userRepository.save(user);
+
+        RefreshToken rt = new RefreshToken("token", user.getUuid(), "127.0.0.1",
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64)", "web", 0);
+        RefreshToken rt2 = new RefreshToken("token", user.getUuid(), "127.0.0.1",
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64)", "web", 0);
+
+        refreshTokenRepository.save(rt);
+        refreshTokenRepository.save(rt2);
+
+        refreshTokenRepository.deleteByUserUuid(user.getUuid());
+
+        List<RefreshToken> found = refreshTokenRepository.findAllByUserUuidAndActiveTrue(user.getUuid());
+
+        assertTrue(found.isEmpty());
+
+    }
+
+    @Test
+    void shouldNotDeleteByUserUuidIfNotExist() {
+        Optional<RefreshToken> found = refreshTokenRepository.findByRefreshToken("non-existent-token");
+
+        assertFalse(found.isPresent());
+    }
+}
