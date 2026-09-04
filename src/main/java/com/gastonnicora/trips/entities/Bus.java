74f0c99a784f12bd0f4fb8fcff557d418892d49e
gastonnicora <gastonmatias.21@gmail.com>
2026-09-04 12:06:56 -0300

    Add Bus entity with attributes for bus management

---
+++ src/main/java/com/gastonnicora/trips/entities/Bus.java
@@ -0,0 +1,95 @@
+package com.gastonnicora.trips.entities;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.UuidGenerator;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Representa un autobús.
+ * <p>
+ * Contiene información sobre el autobús, como su identificador, modelo,
+ * capacidad y estado.
+ * </p>
+ * <p>
+ * Campos principales:
+ * </p>
+ * <ul>
+ * <li>{@code uuid}: Identificador único del autobús.</li>
+ * <li>{@code company}: Empresa a la que pertenece el autobús.</li>
+ * <li>{@code model}: Modelo del autobús.</li>
+ * <li>{@code capacity}: Capacidad del autobús.</li>
+ * <li>{@code status}: Estado del autobús.</li>
+ * <li>{@code createdAt}: Fecha de creación del autobús.</li>
+ * <li>{@code updatedAt}: Fecha de última actualización del autobús.</li>
+ * <li>{@code active}: Indica si el autobús está activo.</li>
+ * </ul>
+ *
+ * <p>
+ * Se utiliza para la gestión de autobuses.
+ * </p>
+ *
+ * @author Gastón
+ * @version 1.0
+ * @since 2026-05-20
+ */
+@Entity
+@Table(name = "buses")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Bus {
+
+    @Id
+    @Column(name = "uuid", nullable = false, unique = true)
+    @GeneratedValue
+    @UuidGenerator
+    private UUID uuid;
+
+    @ManyToOne(optional = false)
+    @Column(name = "company_uuid", nullable = false)
+    private Company company;
+
+    @Column(name = "model", nullable = false)
+    private String model;
+
+    @Column(name = "capacity", nullable = false)
+    private Integer capacity;
+
+    @Column(name = "status", nullable = false)
+    private String status;
+
+    @Column(name = "created_at", nullable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @Column(name = "active", nullable = false)
+    private Boolean active;
+
+
+    public Bus(Company company, String model, Integer capacity, String status) {
+        this.company = company;
+        this.model = model;
+        this.capacity = capacity;
+        this.status = status;
+        this.active = true;
+    }
+}
