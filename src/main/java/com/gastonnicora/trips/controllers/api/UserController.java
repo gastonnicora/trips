c8f49caa36b8d18f4d10b0cbf1c489936ce9aac5
gastonnicora <gastonmatias.21@gmail.com>
2026-09-05 14:19:32 -0300

    Remove getWorkersByUser endpoint and its associated documentation

--- src/main/java/com/gastonnicora/trips/controllers/api/UserController.java
+++ src/main/java/com/gastonnicora/trips/controllers/api/UserController.java
@@ -314,34 +314,4 @@ public class UserController {
         return userService.getWorkersByCurrentUser();
     }
 
-    /**
-     * Obtiene los trabajos asociados a un usuario por su UUID.
-     * <p>
-     * <strong>Requiere autenticación y autorización</strong>
-     * </p>
-     * <p>
-     * <strong>Importante:</strong> Este endpoint solo es accesible para
-     * usuarios con roles "ADMIN", "SUPER_ADMIN" o "HR_MANAGER".
-     * </p>
-     * <p>
-     * Este endpoint obtiene los trabajos asociados a un usuario por su UUID. Se
-     * realiza la validación de los datos antes de obtener los trabajos.
-     * </p>
-     * <p>
-     * Este endpoint hace uso del servicio {@link UserService} para obtener los
-     * trabajos del usuario especificado.
-     * </p>
-     *
-     * @param uuid ({@link UUID}) del usuario a obtener.
-     * @return {@link WorkersByUser} con los trabajos del usuario especificado.
-     * @see UserService #getWorkersByUser(UUID)
-     */ 
-    @GetMapping("/workers/{uuid}")
-    @SecurityRequirement(name = "bearerAuth")
-    @PreAuthorize("hasAnyRole('ADMIN','SUPER_ADMIN','HR_MANAGER')")
-    @Operation(summary = "Obtener trabajos de un usuario", description = "Obtiene los trabajos asociados a un usuario por su uuid")
-    public WorkersByUser getWorkersByUser(@PathVariable UUID uuid) {
-        return userService.getWorkersByUser(uuid);
-    }
-
 }
